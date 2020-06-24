6da14814fd4ec03b4c5f39e089834456931a763f
Jhosep <jhosephgp@gmail.com>
2020-06-24 02:38:22 -0400

    tengo sueño

--- functions/routes/firebase.js
+++ functions/routes/firebase.js
@@ -1,13 +1,13 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-// Development Credentials
-   var serviceAccount = require("../../pataconesenlinea-f3a50-firebase-adminsdk-coa3o-f391094d0f.json");
-   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-     databaseURL: "https://pataconesenlinea-f3a50.firebaseio.com",
-     storageBucket: "gs://pataconesenlinea-f3a50.appspot.com"
-   });
+// // Development Credentials
+    var serviceAccount = require("../../pataconesenlinea-f3a50-firebase-adminsdk-coa3o-f391094d0f.json");
+    admin.initializeApp({
+     credential: admin.credential.cert(serviceAccount),
+      databaseURL: "https://pataconesenlinea-f3a50.firebaseio.com",
+      storageBucket: "gs://pataconesenlinea-f3a50.appspot.com"
+    });
 
 // Production Credentials
 //admin.initializeApp(functions.config().firebase);
