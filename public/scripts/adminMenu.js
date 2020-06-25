bb8c96d2ee7ee5b5791d0567a5272da484d6516f
Jhosep <jhosephgp@gmail.com>
2020-06-24 20:59:58 -0400

    Añado el icono a la pagina de admin

--- public/scripts/adminMenu.js
+++ public/scripts/adminMenu.js
@@ -1,14 +1,22 @@
 let menuButton = document.getElementById('menu_button');
 let menu = document.getElementById('alternative-menu');
 let menuNormal = document.getElementById('menuNormal');
+let startLogo = document.getElementById('startLogo');
 
 menuButton.addEventListener('click', () => {
-    console.log(getComputedStyle(menu).display);
     if(getComputedStyle(menu).display === 'none'){
-        menuNormal.classList.toggle('active'); 
+        menuNormal.classList.toggle('active');
+
+        if(startLogo.classList.contains('active')){
+            startLogo.src = '/img/Devsktop-icon.png'
+        }else{
+            startLogo.src = '/img/Devsktop-logo.png';
+        }
+   
+        startLogo.classList.toggle('active')
+
     }else{
         menu.classList.toggle('active');
        
     }
-    
 });
