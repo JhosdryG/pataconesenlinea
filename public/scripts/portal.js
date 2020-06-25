d548fdb55a235acc4fbc858b1753ea26b18036fc
Jhosep <jhosephgp@gmail.com>
2020-06-24 23:26:56 -0400

    Se arregla el problema de la clase incart

--- public/scripts/portal.js
+++ public/scripts/portal.js
@@ -1,31 +1,34 @@
 const currentCart = JSON.parse(localStorage.getItem('cart'));
 
+// First render of incart products
 if(currentCart){
     Object.keys(currentCart).forEach( key => {
-        $(key).classList.add('incar');
+        $(key).classList.add('incart');
         $q(`#${key} button`).disabled = true;
+        $q(`#${key} button`).innerText = 'Ya en el carrito';
     });
 }
 
-const cantInput = document.getElementById('productCant');
-const portalTotal = document.getElementById('portalTotal');
-const portalAccept = document.getElementById('portalAccept');
-const portalClose = document.getElementById('portalClose');
-const portal = document.getElementById('portal');
+const cantInput = $('productCant');
+const portalTotal = $('portalTotal');
+const portalAccept = $('portalAccept');
+const portalClose = $('portalClose');
+const portal = $('portal');
 
+
+// Validates only numbers
 cantInput.addEventListener('keypress', (e) => {
     const key = e.key;
     const noNumberRegex = /^[0-9]*$/;
-    
 
     if(!noNumberRegex.test(key) && e.keyCode !== 8){
         e.preventDefault();
         return null;
     }
-
-
 });
 
+
+// Extra validations and calc of the price
 cantInput.addEventListener('keyup', (e) => {
     let total = 0;
     let cant = parseInt(cantInput.value, 10);
@@ -52,6 +55,7 @@ portalClose.addEventListener('click', () => {
     portal.classList.toggle('active');
 });
 
+// Triggers when a product is bought
 portalAccept.addEventListener('click', () => {
 
     if(cantInput.value.trim().length > 0 && parseInt(cantInput.value,10) > 0){    
@@ -71,7 +75,8 @@ function addToCart(){
 function updateInterface(){
     portal.classList.toggle('active');
     $q(`#${currentProduct} button`).disabled = true;
-    $(currentProduct).classList.toggle('incar');
+    $q(`#${key} button`).innerText = 'Ya en el carrito';
+    $(currentProduct).classList.toggle('incart');
 }
 
 function updateCartCount(){
